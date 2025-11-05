a8a98676087ac41b7928f8f47d7344281e5c5e18
renatooo2k <rdmilsoni@gmail.com>
2025-11-05 20:27:53 -0300

    Corrige build.gradle.kts para compatibilidade do pipeline

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -26,8 +26,9 @@ android {
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
-        versionCode = flutter.versionCode
+        versionCode = flutter.versionCode.toInt()
         versionName = flutter.versionName
+
     }
 
     buildTypes {
